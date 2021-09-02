10a4011e00c2820ba432132e549fcb0ef3a1f50e
ChildOfFuture <dimp89@mail.ru>
2021-09-02 21:34:33 +0300

    Последние правки первого учебного задания)

--- xoProject/main.py
+++ xoProject/main.py
@@ -1,17 +1,19 @@
+# Переменные для хранения параметров игрового поля
 width = 0
 height = 0
 lineSize = 0
 field = []
-players = [{'name': 'Player 1', 'sign': 'X'},
-           {'name': 'Player 2', 'sign': 'O'}]
+players = [{'name': 'Игрок 1', 'sign': 'X'},
+           {'name': 'Игрок 2', 'sign': 'O'}]
 
+# Функция инициализирующая параметры игры (размер поля, длину линии крестиков-ноликов для победы)
 def initField():
     flag = False
     global field
     global width
     global height
     global lineSize
-    lst = input("enter field params in format <width>x<height>x<linesize> (sample: 3x3x3):").split('x')
+    lst = input("Введите параметры игрового поля в формате <ширина>x<высота>x<число знаков для победы> (пример: 3x3x3):").split('x')
     if len(lst) == 3:
         mask = "1234567890"
         if all(i in mask for i in lst[0]):
@@ -21,7 +23,7 @@ def initField():
                     height = int(lst[1])
                     lineSize = int(lst[2])
                     if width >= lineSize or height >= lineSize:
-                        print("field size:", width, height)
+                        print("Размер поля:", width, height)
                         field.clear()
                         for i in range(width):
                             row = []
@@ -30,18 +32,18 @@ def initField():
                             field.append(row)
                         flag = True
                     else:
-                        print("height or width less than lineSize")
+                        print("размер поля не соответствует числу знаков для победы")
             else:
-                print("invalid height")
+                print("Неверное значение высоты")
         else:
-            print("invalid width")
+            print("Неверное значение ширины")
     else:
-        print("invalid format")
+        print("Неверный формат данных")
     return flag
 
+# Функция выполняющая ход игрока
 def makeMovement(player):
     flag = False
-    global field
     lst = input(player.get('name') + " : ").split(' ')
     mask = "1234567890"
     if len(lst) == 2 and all(i in mask for i in lst[0]) and all(i in mask for i in lst[1]):
@@ -52,31 +54,36 @@ def makeMovement(player):
                 field[row][column] = player.get('sign')
                 flag = True
             else:
-                print("cell (%d %d) is not available", row-1, column-1)
+                print("Клетка (", row+1, ";", column+1, ") занята")
         else:
-            print("wrong cell number")
+            print("Неверный номер клетки")
     else:
-        print("wrong command. Enter cell number in format <row> <column> (sample: 1 1)")
+        print("Неверная команда. Введите номер клетки в формате <номер строки> <номер столбца> (пример: 1 1)")
     return flag
 
+# Функция возвращает все диагонали поля (прим. гравная диагональ возвращается дважды)
 def diagonals(a, b, c):
     res = []
-    global field
     for i in range(a, b, c):
-        lst = []
-        n = i
-        for row in field:
-            if 0 <= n <= width - 1:
-                lst.append(row[n])
-                n += c
-        res.append(lst)
-    print (res)
+        n = 0
+        m = i
+        tmp1 = []
+        tmp2 = []
+        while 0 <= n <= height - 1 and 0 <= m <= width - 1:
+            tmp1.append(field[n][m])
+            tmp2.append(field[m][n])
+            n += 1
+            m += c
+        res.append(tmp1)
+        res.append(tmp2)
     return res
 
+# Функция проверки линии (горизонтальной, вертикальной, диагональной) на наличие победной комбинации
 def checkLine(line, sign):
     strLine = ''.join(list(map(str, line)))
     return sign * lineSize in strLine
 
+# Функция проверки поля на наличие победной комбинации
 def checkField(sign):
     for row in field:
         if checkLine(row, sign):
@@ -87,7 +94,7 @@ def checkField(sign):
         if checkLine(column, sign):
             return True
 
-    for diag in diagonals(0, width - 1, 1):
+    for diag in diagonals(0, width, 1):
         if checkLine(diag, sign):
             return True
 
@@ -97,36 +104,51 @@ def checkField(sign):
 
     return False
 
+# Вспомогательная функция для расчета размера клетки
+def cellSize(n):
+    if n < 10:
+        return 1
+    else:
+        return 1 + cellSize(n // 10)
+
+# Функция вывода поля на экран
 def printField():
-    lst = [str(i) for i in range(1, width+1)]
-    header = "# |" + '|'.join(lst) + "|"
+    cell = cellSize(max(width, height))
+    if cell % 2 == 0:
+        cell += 1
+    lst = [str(i).rjust(cell,' ') for i in range(1, width+1)]
+    header = str("#").rjust(cell,' ') + " |" + '|'.join(lst) + "|"
     print(header)
     for i in range(height):
-        line = str(i+1) + " |" + "|".join(field[i]) + "|"
+        row = [j.center(cell, '_') for j in field[i]]
+        line = str(i+1).rjust(cell,' ') + " |" + "|".join(row) + "|"
         print(line)
     print("-"*len(header))
 
+# Функция-генератор. Попеременно возвращает информацию о игроках, чей ход предстоит
 def gen():
     while True:
         for i in range(len(players)):
             yield i
 
+# Функция, реализующая алгоритм игры
 def game():
-    print("this is the game X O")
+    print("Игра крестики-нолики")
     while not initField():
         continue
-    print("first player - X, second player - O.\nEnter cell number in format <row> <column> (sample: 1 1)\nThe game started!")
+    print("Игрок 1 - X, Игрок 2 - О.\nВведите номер клетки в формате <номер строки> <номер столбца> (пример: 1 1)\nИгра началась!")
     printField()
     for i in gen():
         while not makeMovement(players[i]):
             continue
         printField()
         if checkField(players[i].get('sign')):
-            print(players[i].get('name') + " won!")
+            print(players[i].get('name') + " выиграл!")
             break
         if not any('_' in field[row] for row in range(height)):
-            print("field is full. No more available cells. It`s a draw")
+            print("Поле заполнено. Свободные клетки отсутствуют. Ничья")
             break
-    print("Game over")
+    print("Игра окончена")
 
+# Запуск игры
 game()
